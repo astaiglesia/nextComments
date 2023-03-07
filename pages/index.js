ec12161d2253f2125cd3f7c43f5798564c6b44fb
Alfred Sta Iglesia <75825642+astaiglesia@users.noreply.github.com>
2023-03-07 15:59:02 -0500

    add post overview + util comments

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
 import Head from 'next/head';
+
 import Layout, { siteTitle } from '../components/Layout/Layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+// by default - get static props runs at build time to populate data for static generation of page's html
+// - function retrieves external data and assigns to props 
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+// component is passed allPostsData prop values assigned by getStaticProps
+export default function Home({ allPostsData }) {
+  console.log(allPostsData);
+
   return (
     <Layout home>
       <Head>
@@ -11,11 +28,24 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>Welcome everyone! </p>
         <p>I'm a software engineer developing JS web applications in React with Next. You can contact me on LinkedIn.</p>
-
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
